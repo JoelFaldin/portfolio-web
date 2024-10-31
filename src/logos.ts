9cbf5c48648b7b9bc03ea6401e52cc53ee96cbbc
JoelFaldin <joelfaldin@gmail.com>
2024-10-31 00:50:52 -0300

    refactor: updating profile pic, general improvements

--- src/logos.ts
+++ src/logos.ts
@@ -3,7 +3,6 @@ import TypeScriptLogo from "./components/logos/TS.astro"
 import TailwindLogo from "./components/logos/Tailwind.astro"
 import ViteLogo from "./components/logos/Vite.astro"
 import ExpressLogo from "./components/logos/Express.astro"
-import MongoLogo from "./components/logos/Mongo.astro"
 import MySQLogo from "./components/logos/MySQL.astro"
 import NestJS from "./components/logos/NestJS.astro"
 import Prisma from "./components/logos/Prisma.astro"
@@ -30,10 +29,6 @@ const logos = {
   EXPRESS: {
     name: "ExpressJS",
     icon: ExpressLogo
-  },
-  MONGO: {
-    name: "MongoDB",
-    icon: MongoLogo
   },
   MYSQL: {
     name: "MySQL",
