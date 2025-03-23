c7889b05dbbcbca60142b0e8db1057910c7f391e
JoelFaldin <joelfaldin@gmail.com>
2025-03-23 00:03:21 -0300

    feat: adding function to handle form on contact section

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,48 @@
-import type { PropsWithChildren } from "react";
+import type { FormEvent, PropsWithChildren } from "react";
+import { toast } from "react-hot-toast";
+
+const darkModeStyle = {
+    borderRadius: '10px',
+    background: '#333',
+    color: '#fff',
+}
 
 const ContactForm = ({ children }: PropsWithChildren) => {
-    const handleSubmit = () => {
-        console.log('submitting!');
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const data = Object.fromEntries(formData.entries());
+
+        const loading = toast.loading("Saving message...", { style: darkModeStyle });
+
+        try {
+            const response = await fetch('/api/contact', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+            })
+
+            const res = await response.json();
+
+            if (!res.ok) {
+                console.log("Something went wrong: ", res);
+                toast.dismiss(loading);
+                toast.error(res.message, { style: darkModeStyle })
+            } else {
+                toast.dismiss(loading);
+                toast.success("Message submitted!", { style: darkModeStyle });
+            }
+        } catch (error) {
+            console.log("Something went very wrong: ", error);
+            toast.dismiss(loading);
+            toast.error("There was an error on the server, try again later.", { style: darkModeStyle });
+        }
     }
 
     return (
         <div className="lg:col-span-3">
-            <form onSubmit={handleSubmit} className="bg-[#0a0a14] p-8 rounded-2xl border border-[#e6c478]/20">
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+            <form onSubmit={handleSubmit} className="bg-[#0a0a14] flex flex-col p-8 rounded-2xl border border-[#e6c478]/20">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
                     <div className="space-y-2 flex flex-col">
                         <label htmlFor="name" className="text-white font-medium">
                             Name
@@ -49,10 +83,10 @@ const ContactForm = ({ children }: PropsWithChildren) => {
                     />
                 </div>
 
-                <a type="submit" className="bg-[#e6c478] hover:bg-[#d4b366] text-lg text-[#0a0a14] px-6 py-2 rounded-md flex flex-row items-center justify-center cursor-pointer">
+                <button type="submit" className="bg-[#e6c478] hover:bg-[#d4b366] text-lg text-[#0a0a14] px-6 py-2 rounded-md flex flex-row items-center justify-center cursor-pointer">
                     {children}
                     <span className="font-bold">Send Message</span>
-                </a>
+                </button>
             </form>
         </div>
     )
