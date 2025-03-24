82bdb3829c5d287865211193a7b8d1dbc9a2a724
JoelFaldin <joelfaldin@gmail.com>
2025-03-24 18:02:01 -0300

    feat: resetting form when submitted, removing unused consts

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import type { FormEvent, PropsWithChildren } from "react";
+import { useRef, useState, type FormEvent, type PropsWithChildren } from "react";
 import { toast } from "react-hot-toast";
 
 const darkModeStyle = {
@@ -8,12 +8,16 @@ const darkModeStyle = {
 }
 
 const ContactForm = ({ children }: PropsWithChildren) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const form = useRef<HTMLFormElement | null>(null);
+
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const data = Object.fromEntries(formData.entries());
 
         const loading = toast.loading("Saving message...", { style: darkModeStyle });
+        setIsLoading(true);
 
         try {
             const response = await fetch('/api/contact', {
@@ -32,16 +36,20 @@ const ContactForm = ({ children }: PropsWithChildren) => {
                 toast.dismiss(loading);
                 toast.success("Message submitted!", { style: darkModeStyle });
             }
+
+            setIsLoading(false);
+            form.current?.reset();
         } catch (error) {
             console.log("Something went very wrong: ", error);
             toast.dismiss(loading);
             toast.error("There was an error on the server, try again later.", { style: darkModeStyle });
+            setIsLoading(false);
         }
     }
 
     return (
         <div className="lg:col-span-3">
-            <form onSubmit={handleSubmit} className="bg-[#0a0a14] flex flex-col p-8 rounded-2xl border border-[#e6c478]/20">
+            <form ref={form} onSubmit={handleSubmit} className="bg-[#0a0a14] flex flex-col p-8 rounded-2xl border border-[#e6c478]/20">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
                     <div className="space-y-2 flex flex-col">
                         <label htmlFor="name" className="text-white font-medium">
@@ -83,7 +91,7 @@ const ContactForm = ({ children }: PropsWithChildren) => {
                     />
                 </div>
 
-                <button type="submit" className="bg-[#e6c478] hover:bg-[#d4b366] text-lg text-[#0a0a14] px-6 py-2 rounded-md flex flex-row items-center justify-center cursor-pointer">
+                <button type="submit" disabled={isLoading} className={`${isLoading ? 'bg-[#999999] hover:bg-[#999999] cursor-default' : 'bg-[#e6c478] hover:bg-[#d4b366] cursor-pointer'} text-lg text-[#0a0a14] px-6 py-2 rounded-md flex flex-row items-center justify-center`}>
                     {children}
                     <span className="font-bold">Send Message</span>
                 </button>
