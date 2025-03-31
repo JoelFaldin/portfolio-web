9476ac7c5f59e340f6cfc5e16f397fa308a36e5c
JoelFaldin <joelfaldin@gmail.com>
2025-03-31 19:59:26 -0300

    feat: adding Projects table to database!

--- db/config.ts
+++ db/config.ts
@@ -16,6 +16,17 @@ const Pics = defineTable({
   }
 })
 
+const Projects = defineTable({
+  columns: {
+    title: column.text(),
+    category: column.text(),
+    image: column.text(),
+    enDescription: column.text(),
+    esDescription: column.text(),
+    repo: column.text(),
+  }
+})
+
 export default defineDb({
-  tables: { Messages, Pics },
+  tables: { Messages, Pics, Projects },
 })
