f7bb7dc7e0f6bcc18b30989dae6227ee1a8b04e8
JoelFaldin <joelfaldin@gmail.com>
2026-08-17 22:31:17 -0400

    restart: create empty astro projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
-import db from '@astrojs/db';
-
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [react(), db()],
-  adapter: vercel(),
-
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en',
-    routing: {
-      prefixDefaultLocale: false,
-    }
-  }
-});
+export default defineConfig({});
